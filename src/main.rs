4dd389cefa639e32d97933117c79847b90c9e978
Prabodh Agarwal <prabodh1194@gmail.com>
2022-04-02 18:15:52 +0530

    implemented the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 extern crate core;
 
+use rand::Rng;
 use std::io::stdin;
 
 fn main() {
     println!("Guess the number game!");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
     println!("Enter the number");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => panic!("{}", e),
+        };
+        println!("Guessed number {}", guess);
 
-    let guess: f32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(e) => panic!("{}", e),
-    };
-
-    println!("Guessed number {}", guess);
+        if guess == secret_number {
+            println!("You win!");
+            break;
+        } else if guess > secret_number {
+            println!("Too high!");
+        } else {
+            println!("Too low!");
+        }
+    }
 }
