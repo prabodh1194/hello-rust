edcb957562a2043a2df44ecd91d8cc84e23a8f4f
Prabodh Agarwal <prabodh1194@gmail.com>
2022-04-02 18:20:30 +0530

    move guessing game to a different file

---
+++ src/guessing_game.rs
@@ -0,0 +1,31 @@
+use std::io::stdin;
+use rand::Rng;
+
+pub fn guessing_game() {
+    println!("Guess the number game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Enter the number");
+
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => panic!("{}", e),
+        };
+        println!("Guessed number {}", guess);
+
+        if guess == secret_number {
+            println!("You win!");
+            break;
+        } else if guess > secret_number {
+            println!("Too high!");
+        } else {
+            println!("Too low!");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
 extern crate core;
 
-use rand::Rng;
-use std::io::stdin;
+
+mod guessing_game;
 
 fn main() {
-    println!("Guess the number game!");
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("Enter the number");
-
-    loop {
-        let mut guess = String::new();
-        stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(e) => panic!("{}", e),
-        };
-        println!("Guessed number {}", guess);
-
-        if guess == secret_number {
-            println!("You win!");
-            break;
-        } else if guess > secret_number {
-            println!("Too high!");
-        } else {
-            println!("Too low!");
-        }
-    }
+    guessing_game::guessing_game()
 }
