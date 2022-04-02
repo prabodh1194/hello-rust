da6f7f084b93ab0f1b3336c7b38be297e82053a6
Prabodh Agarwal <prabodh1194@gmail.com>
2022-04-02 17:59:58 +0530

    create a mother of rudimentary programs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate core;
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number game!");
+
+    println!("Enter the number");
+
+    let mut guess = String::new();
+
+    stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: f32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(e) => panic!("{}", e),
+    };
+
+    println!("Guessed number {}", guess);
 }
